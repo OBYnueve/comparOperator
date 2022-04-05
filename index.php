3a6c6d1477f7f09a41d432fc086fff8de383db67
Dylan D'antonio <dyland.antonio.dev42@gmail.com>
2022-04-05 13:48:37 +0200

    modification 13h50

--- index.php
+++ index.php
@@ -1,21 +1,11 @@
 <?php
-require "./utils/ConnectDataBase.php";
 require "./utils/AutoLoad.php";
 Autoloader::register();
-require_once "./utils/InternetProtocol.php";
-
 $manager = new Manager();
-
-
-$arguments = $connect->query("SELECT * FROM destination");
-$arguments->execute();
-$result = $arguments->fetchAll(PDO::FETCH_ASSOC);
-var_dump($result);
-$destination = new Destination($result);
+$tableauDestination = $manager->getAllDestination();
 
 ?>
 <!DOCTYPE html>
-
 <html lang="fr">
 
 <head>
@@ -42,28 +32,28 @@ $destination = new Destination($result);
     </header>
 
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" style="width: 100%;">
-  <div class="carousel-inner">
-    <div class="carousel-item active" style="width:100%; height : auto; ">
-      <img src="img/475457-Los-Angeles.webp" class="d-block w-100" alt="Los-Angeles">
+        <div class="carousel-inner">
+            <div class="carousel-item active" style="width:100%; height : auto; ">
+                <img src="img/475457-Los-Angeles.webp" class="d-block w-100" alt="Los-Angeles">
+            </div>
+            <div class="carousel-item" style="width:100%; height:auto;">
+                <img src="img/Italie-paysage-©-iStock-1093694146.jpg" class="d-block w-100" alt="Italie">
+            </div>
+            <div class="carousel-item" style="width:100%; height:auto;">
+                <img src="img/moraine-lake.1473669.c375x224.jpg" class="d-block w-100" alt="canada">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
-    <div class="carousel-item" style="width:100%; height:auto;">
-      <img src="img/Italie-paysage-©-iStock-1093694146.jpg" class="d-block w-100" alt="Italie">
-    </div>
-    <div class="carousel-item" style="width:100%; height:auto;">
-      <img src="img/moraine-lake.1473669.c375x224.jpg" class="d-block w-100" alt="canada">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
     <!-- barre de recherche -->
-            <form method="post" id="barre">
+    <form method="post" id="barre">
         <div class="form-group">
             <label for="provenance">Ville de départ</label>
             <input type="text" name="provenance" id="provenance" placeholder="Paris, Milan, ...">
@@ -112,145 +102,47 @@ $destination = new Destination($result);
             <input type="submit" value="Lancer la recherche" name="recherche" id="recherche">
         </div>
     </form>
-     <!-- barre de recherche -->
+    <!-- barre de recherche -->
     </div>
     <div class="container">
-        <div class="row wrap p-5 justify-content-between "style = "width: 100%;">
+        <div class="row wrap p-5 justify-content-between " style="width: 100%;">
 
-<?php foreach($arguments as $argument){ ?>
+            <?php for ($i = 0; $i < count($tableauDestination); $i++) { ?>
 
-    <div class="card p-3 m-3" style="width: 18rem;">
-                <img src="./img/khao-sok-parc-thailande.1533479.jpeg" class="card-img-top" alt="thailande">
-                <div class="card-body">
-                <div class="overlay">
-                        <span class="id"><? $argument["id"]; ?></span>
-                        <span class="prix"><? $argument["price"]; ?></span>
-                        <span class="nom"><? $argument["location"]; ?></span>
+                <div class="card p-3 m-3" style="width: 18rem;">
+                    <img src="./img/khao-sok-parc-thailande.1533479.jpeg" class="card-img-top" alt="thailande">
+                    <div class="card-body">
+                        <div class="overlay">
+                            <span class="id"><?= $tableauDestination[$i]->getId(); ?></span>
+                            <span class="prix"><?= $tableauDestination[$i]->getPrice(); ?></span>
+                            <span class="nom"><?= $tableauDestination[$i]->getLocation(); ?></span>
+                        </div>
+                        <p class="card-text"></p>
+                        <button type="button" class="btn btn-dark"><? $argument["tour_operator_id"]; ?></button>
                     </div>
-                    <p class="card-text"></p>
-                    <button type="button" class="btn btn-dark"><? $argument["tour_operator_id"]; ?></button>
                 </div>
-            </div>
 
-<?php } ?>
-            <!-- Thailande -->
-            <div class="card p-3 m-3" style="width: 18rem;">
-                <img src="./img/khao-sok-parc-thailande.1533479.jpeg" class="card-img-top" alt="thailande">
-                <div class="card-body">
-                <div class="overlay">
-                        <span class="prix">Dès 80€</span>
-                        <span class="nom">Thailande</span>
-                    </div>
-                    <p class="card-text"></p>
-                    <button type="button" class="btn btn-dark">Buy</button>
-                </div>
-            </div>
-            <!-- Japon -->
-            <div class="card p-3 m-3" style="width: 18rem;">
-                <img src="./img/(Image)-image-Japon-Fujiyoshida-Pagode-de-Chureito-et-mont-Fuji-au-printemps-as_284756420.jpg" class="card-img-top" alt="thailande">
-                <div class="card-body">
-                <div class="overlay">
-                        <span class="prix">Dès 170€</span>
-                        <span class="nom">Japon</span>
-                    </div>
-                    <p class="card-text"></p>
-                    <button type="button" class="btn btn-dark">Buy</button>
-                </div>
-            </div>
-            <!-- Italie -->
-            <div class="card p-3 m-3" style="width: 18rem;">
-                <img src="./img/Italie-paysage-©-iStock-1093694146.jpg" class="card-img-top" alt="thailande">
-                <div class="card-body">
-                <div class="overlay">
-                        <span class="prix">Dès 440€</span>
-                        <span class="nom">Italie</span>
-                    </div>
-                    <p class="card-text"></p>
-                    <button type="button" class="btn btn-dark">Buy</button>
-                </div>
-            </div>
-            <!-- Los Angeles -->
-            <div class="card p-3 m-3" style="width: 18rem;">
-                <img src="./img/475457-Los-Angeles.webp" class="card-img-top" alt="thailande">
-                <div class="card-body">
-                <div class="overlay">
-                        <span class="prix">Dès 600€</span>
-                        <span class="nom">Los Angeles</span>
-                    </div>
-                    <p class="card-text"></p>
-                    <button type="button" class="btn btn-dark">Buy</button>
-                </div>
-            </div>
-            <!-- New Mexique -->
-            <div class="card p-3 m-3" style="width: 18rem;">
-                <img src="./img/Property-5a7d35d0be1d5c3b6bf9628b6f851ba5-92626705.jpg" class="card-img-top" alt="thailande">
-                <div class="card-body">
-                <div class="overlay">
-                        <span class="prix">Dès 700€</span>
-                        <span class="nom">New Mexique</span>
-                    </div>
-                    <p class="card-text"></p>
-                    <button type="button" class="btn btn-dark">Buy</button>
-                </div>
-            </div>
-            <!-- Brésil -->
-            <div class="card p-3 m-3" style="width: 18rem;">
-                <img src="./img/téléchargement.jpg" class="card-img-top" alt="thailande">
-                <div class="card-body">
-                <div class="overlay">
-                        <span class="prix">Dès 500€</span>
-                        <span class="nom">Brésil</span>
-                    </div>
-                    <p class="card-text"></p>
-                    <button type="button" class="btn btn-dark">Buy</button>
-                </div>
-            </div>
-            <!-- France -->
-            <div class="card p-3 m-3" style="width: 18rem;">
-                <img src="./img/téléchargement (1).jpg" class="card-img-top" alt="thailande">
-                <div class="card-body">
-                <div class="overlay">
-                        <span class="prix">Dès 100€</span>
-                        <span class="nom">France</span>
-                    </div>
-                    <p class="card-text"></p>
-                    <button type="button" class="btn btn-dark">Buy</button>
-                </div>
-            </div>
-            <!-- Canada -->
-            <div class="card p-3 m-3" style="width: 18rem;">
-                <img src="./img/moraine-lake.1473669.c375x224.jpg" class="card-img-top" alt="thailande">
-                <div class="card-body">
-                <div class="overlay">
-                        <span class="prix">Dès 700€</span>
-                        <span class="nom">Canada</span>
-                    </div>
-                    <p class="card-text"></p>
-                    <button type="button" class="btn btn-dark">Buy</button>
-                </div>
-            </div>
-        </div>
-    </div>
+            <?php } ?>
 
-
-    <div class="footer-basic">
-        <footer>
-            <div class="social">
-                <a href="#"><i class="fa fa-instagram"></i></a>
-                <a href="#"><i class="fa fa-snapchat"></i></a>
-                <a href="#"><i class="fa fa-twitter"></i></a>
-                <a href="#"><i class="fa fa-facebook"></i></a>
+            <div class="footer-basic">
+                <footer>
+                    <div class="social">
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="#"><i class="fa fa-snapchat"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                    </div>
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><a href="#">Home</a></li>
+                        <li class="list-inline-item"><a href="#">Services</a></li>
+                        <li class="list-inline-item"><a href="#">About</a></li>
+                        <li class="list-inline-item"><a href="#">Terms</a></li>
+                        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+                    </ul>
+                    <p class="copyright">Company ComparOperator © 2022</p>
+                </footer>
             </div>
-            <ul class="list-inline">
-                <li class="list-inline-item"><a href="#">Home</a></li>
-                <li class="list-inline-item"><a href="#">Services</a></li>
-                <li class="list-inline-item"><a href="#">About</a></li>
-                <li class="list-inline-item"><a href="#">Terms</a></li>
-                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
-            </ul>
-            <p class="copyright">Company ComparOperator © 2022</p>
-        </footer>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
+
 </html>
